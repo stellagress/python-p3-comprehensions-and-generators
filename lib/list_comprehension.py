99be32dd0e84bdf4934a514def091f5a2d5d4215
Stella Mazzillo <stellagress5@gmail.com>
2023-07-07 23:33:01 -0400

    complete p3 comprehension and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,59 @@
 #!/usr/bin/env python3
 
+# def return_evens(num_list):
+#     even_num=[]
+#     for n in num_list:
+#         if n % 2 == 0:
+#             even_num.append(n)
+#     return even_num
+
+
 def return_evens(num_list):
-    pass
+    even_num=[]
+    for n in num_list:
+        if n % 2 == 0:
+            even_num.append(n)
+    return even_num
+    
+
+
+# def return_evens(num_list):
+#     return [(n % 2 == 0) for n in num_list]
+
+
+
+
+
+# def make_exclamation(sentence_list):
+#     words = []
+
+#     while len(words) <  len(sentence_list):
+#         words.append("!")
+#     return words
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_point = []
+
+    for s in sentence_list:
+        exclamation_point.append(s + "!")
+        
+    return exclamation_point
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
